389067c946418d91f1876016423a1dc76acf6e85
Anatoly Sadowsky <sadowsky83@gmail.com>
2016-12-18 15:54:06 +0700

    Поправил графику

---
+++ js/mobilemenu.js
@@ -0,0 +1,16 @@
+var navMain = document.querySelector('.main-navigation');
+var navOpen = document.querySelector('.open-btn');
+var navClose = document.querySelector('.close-btn');
+
+navMain.classList.remove('main-navigation--nojs');
+navOpen.classList.remove('button--nojs');
+navClose.classList.remove('button--nojs');
+
+navOpen.addEventListener('click', function() {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+});
+navClose.addEventListener('click', function() {
+    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add('main-navigation--closed');
+  });
